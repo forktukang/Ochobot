084ed5dd8f39dd8a662714add173fe04a3f3dbe8
REYZU <reyzuproject@gmail.com>
2022-11-06 08:15:59 +0700

    Update misc.py

--- ReyzuRobot/modules/helper_funcs/misc.py
+++ ReyzuRobot/modules/helper_funcs/misc.py
@@ -72,7 +72,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         pairs.append((modules[-1],))
 
     else:
-        pairs += [[EqInlineKeyboardButton("Get Developer Info and Module Help", url=f"https://t.me/ReyzuSupport")]]
+        pairs += [[EqInlineKeyboardButton("Get Developer Info and Module Help", url=f"https://t.me/renaldivanhoten")]]
 
     return pairs
 
